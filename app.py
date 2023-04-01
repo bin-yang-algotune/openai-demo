43b19cf0f2bbd0e0fcc81cdf96c350b1c7c56f0e
Bin Yang <bin.yang@algotune.com>
2023-03-31 20:40:42 -0400

    add cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from wb_embedding import ask_wb_question
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/ask', methods=['POST'])
